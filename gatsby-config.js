16265314ee3424318d99fcdfa92c4bade3c8484c
Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>
2021-08-24 22:58:16 +0600

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteMetadata: {
+    title: `Aigerim Suleimenova | Web Developer`,
+    author: `Aigerim Suleimenova`,
+    description: `Stefan Pop Fullstack Web Developer portfolio`,
+    siteUrl: `https://www.stefanpop.dev`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-158673617-1',
+        head: true,
+        anonymize: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    {
+      resolve: `@sentry/gatsby`,
+      options: {
+        dsn: `https://2970193c67834319884921bbccf060cf@o456775.ingest.sentry.io/5450147`,
+        sampleRate: 0.7,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-dark-mode`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          
+           `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      },
+    },
+    {
+      resolve: `gatsby-transformer-sharp`,
+      options: {
+        checkSupportedExtensions: false,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Stefan Pop Portfolio`,
+        short_name: `Stefan Pop`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#02aab0`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`,
+      },
+    },
+  ],
+};
